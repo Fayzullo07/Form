44ac9e76363b1c9ad6481740ca1997b97db0366e
Fayzullo07 <91960612+Fayzullo07@users.noreply.github.com>
2022-03-08 23:31:11 +0500

    used useRef

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      card: "",
+      cv: "",
+      name: "",
+  };
+  this.cardRef = React.createRef();
+  this.cvRef = React.createRef();
+  this.nameRef = React.createRef();
+  }
+
+  
+
+  handleChange = (e) => {
+    this.setState({[e.target.name]: e.target.value}, () => {
+      if(this.state.card.length === 16) {
+        this.cvRef.current.focus();
+      }
+
+      if(this.state.cv.length === 3) {
+        this.nameRef.current.focus();
+      }
+    })
+  }
+
+  componentDidMount() {
+    this.cardRef.current.focus();
+  }
+
+  render () {
+    const { card, cv, name } = this.state;
+
+    return (
+      <div className="App">
+        <div>
+          <input
+            id='card'
+            name='card' 
+            placeholder='VISA Number'
+            type='text'
+            value={card}
+            onChange={this.handleChange}
+            ref={this.cardRef}
+          />
+          <br/>
+
+          <input
+            id='card'
+            name='cv' 
+            placeholder='CV'
+            type='text'
+            value={cv}
+            onChange={this.handleChange}
+            ref={this.cvRef}
+          />
+          <br/>
+          <input
+            id='name'
+            name='name' 
+            placeholder='name'
+            type='text'
+            value={name}
+            onChange={this.handleChange}
+            ref={this.nameRef}
+          />
+          <br/>
+            
+          <br/>
+          <button>Send</button>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
